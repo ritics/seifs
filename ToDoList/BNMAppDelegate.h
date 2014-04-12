10ef74a5c193838eda4c2e74635f7024db6d9db4
Reinis Lācis <Reinis@server.rl>
2014-04-12 13:55:20 +0300

    Initial Commit

---
+++ ToDoList/BNMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNMAppDelegate.h
+//  ToDoList
+//
+//  Created by Reinis Lācis on 12/04/14.
+//  Copyright (c) 2014 Brand Name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
